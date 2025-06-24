9ebd7e239018d0650091cf458b836b27dd651735
allan-hx <allan_qvq@163.com>
2025-06-24 15:51:32 +0800

    修改机器人

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Script from 'next/script'
 export default function Index() {
   return (
     <>
-      <Script src="https://telegram.org/js/telegram-widget.js" />
+      <Script src="/telegram.js" />
       <div className='w-screen h-screen flex items-center justify-center'>
         <Button />
       </div>
