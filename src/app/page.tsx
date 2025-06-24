af105327dba2c21976388130db34b2eabc09f001
allan-hx <allan_qvq@163.com>
2025-06-24 12:29:36 +0800

    自定义引入方式

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
-'use client';
-
-import { useEffect } from "react";
-
-export default function Home() {
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (window as any).onTelegramAuth = function(user: any) {
-      console.log('user:', user);
-    }
-  }, []);
-
+import Script from 'next/script'
+ 
+export default function Index() {
   return (
-    <div className='w-screen h-screen flex align-center justify-center aa' dangerouslySetInnerHTML={{
-      __html: `<script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="Hugo_ovo_bot" data-size="medium" data-userpic="false" data-auth-url="https://example-nine-beige.vercel.app" data-request-access="write"></script>`
-    }}>
-    </div>
+    <>
+      <Script src="https://telegram.org/js/telegram-widget.js" />
+      <div className='button'>Login</div>
+    </>
   )
 }
