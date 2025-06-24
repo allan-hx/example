e724a9537f752eac45ad26b55cda884049363229
allan-hx <allan_qvq@163.com>
2025-06-24 13:31:45 +0800

    修改机器人

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { useCallback } from "react";
 
 export default function Button() {
@@ -7,18 +5,17 @@ export default function Button() {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (window as any).Telegram.Login.auth({
       bot_id: '547043436',
-      origin: 'https://core.telegram.org',
+      origin: 'https://example-eosin.vercel.app',
       embed: '1',
       request_access: 'write',
-      return_to: 'https://core.telegram.org/widgets/login',
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      onAuthCallback(data: any) {
-        console.log('data:', data);
-      }
+      return_to: 'https://example-eosin.vercel.app/',
     });
   }, []);
 
   return (
-    <button className='button' onClick={onLogin}>Login</button>
+    <>
+      <button className='button' onClick={onLogin}>Login</button>
+      <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="Hugo_Web_Test_Bot" data-size="medium" data-userpic="false" data-auth-url="allan-hx" data-request-access="write"></script>
+    </>
   );
 }
